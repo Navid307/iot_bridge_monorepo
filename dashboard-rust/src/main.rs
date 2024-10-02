e8fd03ecf772d080027f611a6944d0ddfc6b34c4
Navid <navid.h307@gmail.com>
2024-10-02 00:01:45 -0400

    Added logger

--- dashboard-rust/src/main.rs
+++ dashboard-rust/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use log::*;
+use std::str;
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info,dashboard_rust=debug");
+    }
+
+    env_logger::init();
+
+    info!("Starting application");
+
+    let git_version_task = tokio::spawn(async {
+        match get_git_version().await {
+            Ok(version) => info!("Git version: {}", version),
+            Err(e) => error!("Failed to get Git version: {}", e),
+        }
+    });
+
+    let _ = tokio::join!(git_version_task);
+}
+
+async fn get_git_version() -> Result<String, Box<dyn std::error::Error>> {
+    let output = Command::new("git")
+        .arg("log")
+        .arg("-1")
+        .arg("--oneline")
+        .output()
+        .await?; // TODO: Replace this with --describe after the first tag
+    if output.status.success() {
+        Ok(str::from_utf8(&output.stdout)?.trim().to_string())
+    } else {
+        Err("Git command failed".into())
+    }
 }
